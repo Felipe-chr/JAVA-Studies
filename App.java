f43a09cd2506bfb57a72bd743365c0ad520c3931
Felipe-chr <felipe003009@gmail.com>
2026-08-12 22:36:14 -0300

    feat: structure project with Person model, record definition, and App entry point
    Implement Person class with fields, constructors, encapsulation, and an age increment placeholder.
    Define a basic immutable record1 instance to showcase Java's data carrier features.
    Connect the main entry point to instantiate, manipulate, and log Person and record objects.

--- App.java
+++ App.java
@@ -1,7 +1,21 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Configurando a JDK na versão  " + args[0]);
-        System.out.println("Programando em " + args[1]);
+
+    public static void main(String[] args){
+
+        var male = new Person("João", 18);
+
+        var female = new Person();
+        female.setName("Maria");
+        female.setAge(16);
+
+        System.out.println("Male name: " + male.getName() + " age: " + male.getAge());
+        System.out.println("Female name: " + female.getName() + " age: " + female.getAge());
     }
 }
+public static void main(String[] args){
+
+    var record1 = new record1("João");
+    System.out.println(record1.name());
+}
+
+
---
+++ record1.java
@@ -0,0 +1,5 @@
+public record record1(String name) {
+
+    public void getName(String name, int age){}
+    
+}
